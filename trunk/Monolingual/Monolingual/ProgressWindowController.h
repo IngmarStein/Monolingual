935b80ccce7fe6c5e5044bdf663f32d10ba7b92a
ingmarstein <ingmarstein@427d0cad-6289-43e0-b85d-425d78545d37>
2004-04-09 08:55:35 +0000

    Initial revision

---
+++ trunk/Monolingual/Monolingual/ProgressWindowController.h
@@ -0,0 +1,42 @@
+/*
+ * monolingual -
+ *  front end for monolingual.pl (included in this package), which
+ *  strips away extra language .lproj from OSX to save space
+ *
+ *   Copyright (C) 2001, 2002 Joshua Schrier (jschrier@mac.com),
+ *   2004 Ingmar Stein
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#import <Cocoa/Cocoa.h>
+#import "MyResponder.h"
+
+@interface ProgressWindowController : NSWindowController
+{
+	IBOutlet id progressBar;
+	IBOutlet id applicationText;
+	IBOutlet id fileText;
+}
+
+- (IBAction) cancelButton: (id)sender;
+- (void) start;
+- (void) stop;
+- (void) setText: (NSString *)text;
+- (void) setFile: (NSString *)file;
+- (void) windowDidLoad;
++ (id) sharedProgressWindowController: (id)sender;
+
+@end
---
+++ trunk/Monolingual/Monolingual/VersionCheck.m
@@ -0,0 +1,87 @@
+/*
+ *  VersionCheck.m
+ *  Copyright (C) 2001, 2002  Joshua Schrier (jschrier@mac.com),
+ *  2004 Ingmar Stein
+ *  Released under the GNU GPL.  For more information, see the header file.
+ */
+
+#import "VersionCheck.h"
+
+@implementation VersionCheck
+
+/* Implemented new version, after being inspired by 
+http://www.cocoadevcentral.com/tutorials/showpage.php?show=00000047.php
+The old way used a perl script, curl, and a NSTask, and was quite ugly;  
+see the Monolingual or Swap Cop source code for how this used to be
+*/
+
++ (void) checkInfrequentVersionAtURL: (NSString *)url displayText: (NSString *)message downloadURL: (NSString *)goURL
+{
+	[VersionCheck checkVersionAtURL: url withDayInterval:30 displayText: message downloadURL: goURL];
+}
+
++ (void) checkVersionAtURL: (NSString *)url withDayInterval: (int)minDays displayText: (NSString *)message downloadURL: (NSString *)goURL
+{
+	NSString *currVersionNumber = [[[NSBundle bundleForClass:[self class]] infoDictionary] objectForKey:@"CFBundleVersion"];
+	NSDictionary *productVersionDict;
+	NSCalendarDate *lastCheck = [[NSUserDefaults standardUserDefaults] objectForKey:@"lastVersionCheckDate"];
+	int days;
+	NSString *latestVersionNumber;
+
+	[[NSCalendarDate calendarDate] 
+		years: NULL months: NULL days: &days hours: NULL minutes: NULL seconds: NULL sinceDate: lastCheck]; 
+
+	if ((lastCheck == nil) || (days> minDays)) {
+		NSLog(@"Going online to check version...");
+		productVersionDict = [NSDictionary dictionaryWithContentsOfURL: [NSURL URLWithString:url]];
+		latestVersionNumber = [productVersionDict valueForKey:
+			[[[NSBundle bundleForClass:[self class]] infoDictionary] objectForKey:@"CFBundleExecutable"] ];
+
+		// do nothing--be quiet if there is no active connection or if the
+		// version number could not be downloaded
+		if( latestVersionNumber ) {
+			if (![latestVersionNumber isEqualToString: currVersionNumber]) {
+				NSBeginAlertSheet(
+							  NSLocalizedString(@"Update Available",@""), NSLocalizedString(@"OK",@""), 
+							  NSLocalizedString(@"Cancel",@""), nil, nil, self, NULL, 
+							  @selector(downloadSelector: returnCode: contextInfo:), goURL, message,nil);
+				[[NSUserDefaults standardUserDefaults] setObject: nil forKey:@"lastVersionCheckDate"];
+			} else {
+				// Everything is fine, update the counter
+				[[NSUserDefaults standardUserDefaults] setObject: [NSCalendarDate calendarDate] forKey:@"lastVersionCheckDate"];
+			}
+		}
+	}
+}
+
++ (void) checkVersionAtURL: (NSString *)url displayText: (NSString *)message downloadURL: (NSString *)goURL
+{
+	NSString *currVersionNumber = [[[NSBundle bundleForClass:[self class]] infoDictionary] objectForKey:@"CFBundleVersion"];
+	NSDictionary *productVersionDict = [NSDictionary dictionaryWithContentsOfURL: [NSURL URLWithString:url]];
+	NSString *latestVersionNumber = [productVersionDict valueForKey:
+		[[[NSBundle bundleForClass:[self class]] infoDictionary] objectForKey:@"CFBundleExecutable"] ];
+
+/*
+	NSLog([[[NSBundle bundleForClass:[self class]] infoDictionary] objectForKey:@"CFBundleExecutable"] );
+	NSLog(currVersionNumber);
+	NSLog(latestVersionNumber);
+*/
+
+	// do nothing--be quiet if there is no active connection or if the
+	// version number could not be downloaded
+	if ((latestVersionNumber != nil) && (![latestVersionNumber isEqualToString: currVersionNumber])) {
+		NSBeginAlertSheet(
+			NSLocalizedString(@"Update Available",@""), NSLocalizedString(@"OK",@""), 
+			NSLocalizedString(@"Cancel",@""), nil, nil, self, NULL, 
+			@selector(downloadSelector: returnCode: contextInfo:), goURL, message,nil);
+	}
+}
+
++ (void) downloadSelector: (NSWindow *)sheet returnCode: (int)returnCode contextInfo: (id)contextInfo
+{
+	if (returnCode == NSAlertDefaultReturn) { 
+		[[NSWorkspace sharedWorkspace] openURL: [NSURL URLWithString: contextInfo]];
+	}
+}
+
+@end
