0aa07222c7e6c967a7ba5a199d1fc28ad02679c9
Ingmar Stein <IngmarStein@users.noreply.github.com>
2006-08-08 07:20:22 +0000

    Added some more cpu types

--- Monolingual/MyResponder.m
+++ Monolingual/MyResponder.m
@@ -1120,7 +1120,7 @@ static void dataCallback(CFSocketRef s, CFSocketCallBackType callbackType,
 	mach_port_deallocate(mach_task_self(), my_mach_host_self);
 
 	[currentArchitecture setStringValue:(NSString *)CFSTR("unknown")];
-	CFMutableArrayRef knownArchitectures = CFArrayCreateMutable(kCFAllocatorDefault, 8, &kCFTypeArrayCallBacks);
+	CFMutableArrayRef knownArchitectures = CFArrayCreateMutable(kCFAllocatorDefault, 9, &kCFTypeArrayCallBacks);
 	for (unsigned i=0U; i<9U; ++i) {
 		CFMutableDictionaryRef architecture = CFDictionaryCreateMutable(kCFAllocatorDefault, 3, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
 		CFDictionarySetValue(architecture, CFSTR("enabled"), (ret == KERN_SUCCESS && (hostInfo.cpu_type != archs[i].cpu_type || hostInfo.cpu_subtype < archs[i].cpu_subtype)) ? kCFBooleanTrue : kCFBooleanFalse);
--- Monolingual/lipo.c
+++ Monolingual/lipo.c
@@ -73,51 +73,62 @@ enum byte_sex {
 					   ((unsigned long)(a) >> 24) )
 
 static const struct arch_flag arch_flags[] = {
-	{ "any",	    CPU_TYPE_ANY,	    CPU_SUBTYPE_MULTIPLE },
-	{ "little",	    CPU_TYPE_ANY,	    CPU_SUBTYPE_LITTLE_ENDIAN },
-	{ "big",	    CPU_TYPE_ANY,	    CPU_SUBTYPE_BIG_ENDIAN },
+	{ "any",	    CPU_TYPE_ANY,	      CPU_SUBTYPE_MULTIPLE },
+	{ "little",	    CPU_TYPE_ANY,	      CPU_SUBTYPE_LITTLE_ENDIAN },
+	{ "big",	    CPU_TYPE_ANY,	      CPU_SUBTYPE_BIG_ENDIAN },
 
 	/* 64-bit Mach-O architectures */
 
 	/* architecture families */
-	{ "ppc64", 	    CPU_TYPE_POWERPC64, CPU_SUBTYPE_POWERPC_ALL },
+	{ "ppc64", 	    CPU_TYPE_POWERPC64,   CPU_SUBTYPE_POWERPC_ALL },
 	/* specific architecture implementations */
-	{ "ppc970-64",  CPU_TYPE_POWERPC64, CPU_SUBTYPE_POWERPC_970 },
+	{ "ppc970-64",  CPU_TYPE_POWERPC64,   CPU_SUBTYPE_POWERPC_970 },
 
 	/* 32-bit Mach-O architectures */
 
 	/* architecture families */
-	{ "ppc",	    CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_ALL },
-	{ "x86",        CPU_TYPE_X86,	    CPU_SUBTYPE_X86_ALL },
-	{ "x86_64",     CPU_TYPE_X86_64,    CPU_SUBTYPE_X86_64_ALL },
-	{ "m68k",       CPU_TYPE_MC680x0,   CPU_SUBTYPE_MC680x0_ALL },
-	{ "hppa",       CPU_TYPE_HPPA,	    CPU_SUBTYPE_HPPA_ALL },
-	{ "sparc",	    CPU_TYPE_SPARC,     CPU_SUBTYPE_SPARC_ALL },
-	{ "m88k",       CPU_TYPE_MC88000,   CPU_SUBTYPE_MC88000_ALL },
-	{ "i860",       CPU_TYPE_I860,	    CPU_SUBTYPE_I860_ALL },
+	{ "ppc",	      CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_ALL },
+	{ "x86",          CPU_TYPE_X86,	      CPU_SUBTYPE_X86_ALL },
+	{ "x86_64",       CPU_TYPE_X86_64,    CPU_SUBTYPE_X86_64_ALL },
+	{ "m68k",         CPU_TYPE_MC680x0,   CPU_SUBTYPE_MC680x0_ALL },
+	{ "hppa",         CPU_TYPE_HPPA,	  CPU_SUBTYPE_HPPA_ALL },
+	{ "sparc",	      CPU_TYPE_SPARC,     CPU_SUBTYPE_SPARC_ALL },
+	{ "m88k",         CPU_TYPE_MC88000,   CPU_SUBTYPE_MC88000_ALL },
+	{ "i860",         CPU_TYPE_I860,	  CPU_SUBTYPE_I860_ALL },
 	/* specific architecture implementations */
-	{ "ppc601",     CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_601 },
-	{ "ppc603",     CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_603 },
-	{ "ppc603e",    CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_603e },
-	{ "ppc603ev",   CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_603ev },
-	{ "ppc604",     CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_604 },
-	{ "ppc604e",    CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_604e },
-	{ "ppc750",     CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_750 },
-	{ "ppc7400",    CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_7400 },
-	{ "ppc7450",    CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_7450 },
-	{ "ppc970",     CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_970 },
-	{ "i486",       CPU_TYPE_I386,	    CPU_SUBTYPE_486 },
-	{ "i486SX",     CPU_TYPE_I386,	    CPU_SUBTYPE_486SX },
-	{ "pentium",    CPU_TYPE_I386,	    CPU_SUBTYPE_PENT }, /* same as i586 */
-	{ "i586",       CPU_TYPE_I386,	    CPU_SUBTYPE_586 },
-	{ "pentpro",    CPU_TYPE_I386,      CPU_SUBTYPE_PENTPRO }, /* same as i686 */
-	{ "i686",       CPU_TYPE_I386,      CPU_SUBTYPE_PENTPRO },
-	{ "pentIIm3",   CPU_TYPE_I386,      CPU_SUBTYPE_PENTII_M3 },
-	{ "pentIIm5",   CPU_TYPE_I386,      CPU_SUBTYPE_PENTII_M5 },
-	{ "pentium4",   CPU_TYPE_I386,      CPU_SUBTYPE_PENTIUM_4 },
-	{ "m68030",     CPU_TYPE_MC680x0,   CPU_SUBTYPE_MC68030_ONLY },
-	{ "m68040",     CPU_TYPE_MC680x0,   CPU_SUBTYPE_MC68040 },
-	{ "hppa7100LC", CPU_TYPE_HPPA,      CPU_SUBTYPE_HPPA_7100LC },
+	{ "ppc601",       CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_601 },
+	{ "ppc603",       CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_603 },
+	{ "ppc603e",      CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_603e },
+	{ "ppc603ev",     CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_603ev },
+	{ "ppc604",       CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_604 },
+	{ "ppc604e",      CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_604e },
+	{ "ppc750",       CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_750 },
+	{ "ppc7400",      CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_7400 },
+	{ "ppc7450",      CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_7450 },
+	{ "ppc970",       CPU_TYPE_POWERPC,   CPU_SUBTYPE_POWERPC_970 },
+	{ "i486",         CPU_TYPE_X86,	      CPU_SUBTYPE_486 },
+	{ "i486SX",       CPU_TYPE_X86,	      CPU_SUBTYPE_486SX },
+	{ "pentium",      CPU_TYPE_X86,	      CPU_SUBTYPE_PENT }, /* same as i586 */
+	{ "i586",         CPU_TYPE_X86,	      CPU_SUBTYPE_586 },
+	{ "pentpro",      CPU_TYPE_X86,       CPU_SUBTYPE_PENTPRO }, /* same as i686 */
+	{ "i686",         CPU_TYPE_X86,       CPU_SUBTYPE_PENTPRO },
+	{ "pentIIm3",     CPU_TYPE_X86,       CPU_SUBTYPE_PENTII_M3 },
+	{ "pentIIm5",     CPU_TYPE_X86,       CPU_SUBTYPE_PENTII_M5 },
+	{ "celeron",      CPU_TYPE_X86,       CPU_SUBTYPE_CELERON },
+	{ "celeronm",     CPU_TYPE_X86,       CPU_SUBTYPE_CELERON_MOBILE },
+	{ "pentium3",     CPU_TYPE_X86,       CPU_SUBTYPE_PENTIUM_3 },
+	{ "pentium3m",    CPU_TYPE_X86,       CPU_SUBTYPE_PENTIUM_3_M },
+	{ "pentium3xeon", CPU_TYPE_X86,       CPU_SUBTYPE_PENTIUM_3_XEON },
+	{ "pentiumm",     CPU_TYPE_X86,       CPU_SUBTYPE_PENTIUM_M },
+	{ "pentium4",     CPU_TYPE_X86,       CPU_SUBTYPE_PENTIUM_4 },
+	{ "pentium4m",    CPU_TYPE_X86,       CPU_SUBTYPE_PENTIUM_4_M },
+	{ "itanium",      CPU_TYPE_X86,       CPU_SUBTYPE_ITANIUM },
+	{ "itanium2",     CPU_TYPE_X86,       CPU_SUBTYPE_ITANIUM_2 },
+	{ "xeon",         CPU_TYPE_X86,       CPU_SUBTYPE_XEON },
+	{ "xeonmp",       CPU_TYPE_X86,       CPU_SUBTYPE_XEON_MP },
+	{ "m68030",       CPU_TYPE_MC680x0,   CPU_SUBTYPE_MC68030_ONLY },
+	{ "m68040",       CPU_TYPE_MC680x0,   CPU_SUBTYPE_MC68040 },
+	{ "hppa7100LC",   CPU_TYPE_HPPA,      CPU_SUBTYPE_HPPA_7100LC },
 	{ NULL,	0,		  0 }
 };
 
