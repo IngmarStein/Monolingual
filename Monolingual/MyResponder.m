6820cbe54a66c9a1d3dccbe5c08686c7f441fe1b
Ingmar Stein <IngmarStein@users.noreply.github.com>
2007-01-04 22:29:37 +0000

    Updated copyright notice for 2007

--- Monolingual/MyResponder.m
+++ Monolingual/MyResponder.m
@@ -1,6 +1,6 @@
 /*
  *  Copyright (C) 2001, 2002  Joshua Schrier (jschrier@mac.com),
- *  2004-2006 Ingmar Stein
+ *  2004-2007 Ingmar Stein
  *  Released under the GNU GPL.  For more information, see the header file.
  */
 
