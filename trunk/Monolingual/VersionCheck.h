4103bd7472d833dac6e4b6b81689a8cc86666405
ingmarstein <ingmarstein@427d0cad-6289-43e0-b85d-425d78545d37>
2007-01-04 22:29:37 +0000

    Updated copyright notice for 2007

--- trunk/Monolingual/VersionCheck.h
+++ trunk/Monolingual/VersionCheck.h
@@ -1,7 +1,7 @@
 /*
 #    VersionCheck.h
 #    Copyright (C) 2001, 2002 Joshua Schrier (jschrier@mac.com),
-#    2004-2006 Ingmar Stein
+#    2004-2007 Ingmar Stein
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
