9e68e6924925a7fc1dbd9c20a820e04856ed7318
Ingmar Stein <IngmarStein@users.noreply.github.com>
2007-08-21 13:04:10 +0000

    Updated NIB-2.0 files to XIB-3.0 Added the ability to specify separate root directories for languages and architectures

---
+++ Monolingual/generate_xib.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+primary=English
+primarydir=$primary.lproj
+for language in Japanese Swedish Polish German French Italian; do
+	langdir=$language.lproj
+	for f in `find $primary.lproj -name \*.xib -not -name \*~.xib -type f`; do
+		xibfile=`basename $f`
+		xibname=`basename $f .xib`
+		stringsfile=$langdir/$xibname.strings
+		if [ -e $stringsfile ]; then
+			translated="$langdir/$xibfile"
+			ibtool --strings-file $langdir/$xibname.strings $primarydir/$xibfile --write $translated
+			echo Updated $langdir/$xibfile
+		else
+			cp $primarydir/$xibfile/*.xib $langdir/$xibfile
+			echo $stringsfile not present, using English $xibfile
+		fi
+	done;
+done
