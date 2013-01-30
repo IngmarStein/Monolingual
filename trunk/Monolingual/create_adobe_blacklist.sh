15e3b813e975078a4cd2ffe67f16e5096c391805
ingmarstein <ingmarstein@427d0cad-6289-43e0-b85d-425d78545d37>
2013-01-30 11:39:13 +0000

    Added script to add bundle ids for all Adobe applications to blacklist

---
+++ trunk/Monolingual/create_adobe_blacklist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cat <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<array>
+EOF
+find /Applications/Adobe* -name Info.plist -print0 | while read -d $'\0' file; do
+	bundleid=`/usr/libexec/PlistBuddy -c Print:CFBundleIdentifier "$file" 2>&1`
+	rc=$?
+	if [[ $rc == 0 ]]; then
+		echo "<dict><key>architectures</key><true/><key>bundle</key><string>$bundleid</string><key>languages</key><true/></dict>"
+	fi
+done
+cat <<EOF
+</array>
+</plist>
+EOF
