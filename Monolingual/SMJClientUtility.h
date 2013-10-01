82f3dc5887ce6afa1e739088d39ed8eba7d97839
Ingmar Stein <IngmarStein@users.noreply.github.com>
2013-10-01 04:46:45 +0000

    Use CocoaPods

--- Monolingual/SMJClientUtility.h
+++
@@ -1,8 +0,0 @@
-@interface SMJClientUtility : NSObject
-
-+ (NSString*) versionForBundlePath:(NSString*)bundlePath;
-+ (NSString*) versionForBundlePath:(NSString*)bundlePath error:(NSError**)error;
-
-+ (AuthorizationRef) authWithRight:(AuthorizationString)rightName prompt:(NSString*)prompt error:(NSError**)error;
-
-@end
