68851ff92a60b3018d215ff686cc2d6c36f99cee
IngmarStein <IngmarStein@users.noreply.github.com>
2015-03-30 22:39:02 +0200

    Move XPCObject.swift to XPCSwift

--- Podfile
+++ Podfile
@@ -3,4 +3,5 @@ platform :osx, '10.10'
 inhibit_all_warnings!
 use_frameworks!
 pod 'SMJobKit', :git => 'https://github.com/IngmarStein/SMJobKit.git'
+pod 'XPCSwift', :git => 'https://github.com/IngmarStein/XPCSwift.git'
 pod 'Sparkle', '~> 1.9.0'
--- compat.h
+++ compat.h
@@ -29,18 +29,6 @@ extern const int kCPU_SUBTYPE_X86_64_H;
 
 extern const mach_msg_type_number_t kHOST_BASIC_INFO_COUNT;
 extern const NSUInteger kXPC_CONNECTION_MACH_SERVICE_PRIVILEGED;
-extern const size_t kXPC_ARRAY_APPEND;
-
-extern const xpc_type_t xpc_type_bool;
-extern const xpc_type_t xpc_type_int64;
-extern const xpc_type_t xpc_type_uint64;
-extern const xpc_type_t xpc_type_string;
-extern const xpc_type_t xpc_type_double;
-extern const xpc_type_t xpc_type_data;
-extern const xpc_type_t xpc_type_array;
-extern const xpc_type_t xpc_type_dictionary;
-extern const xpc_type_t xpc_type_error;
-extern const xpc_type_t xpc_type_connection;
 
 extern const xpc_object_t xpc_error_connection_interrupted;
 extern const xpc_object_t xpc_error_connection_invalid;
