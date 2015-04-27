ae1b6d7a0e3a2339813440a0ab528455eb6612d1
IngmarStein <IngmarStein@users.noreply.github.com>
2015-04-27 13:53:57 +0200

    Add lipo unit test

--- Helper Tests/Helper_Tests.swift
+++ Helper Tests/Helper_Tests.swift
@@ -15,6 +15,10 @@ class Helper_Tests: XCTestCase {
 		return NSFileManager.defaultManager().currentDirectoryPath.stringByAppendingPathComponent("testdata")
 	}
 
+	private var utilDir: String {
+		return NSFileManager.defaultManager().currentDirectoryPath.stringByAppendingPathComponent("util")
+	}
+
 	private func createTestApp(name: String, bundleIdentifier: String) {
 		let appDir = testDir.stringByAppendingPathComponent("\(name).app")
 		let localizableStringsData = NSData(base64EncodedString: "dGVzdA==", options: .allZeros)!
@@ -36,6 +40,11 @@ class Helper_Tests: XCTestCase {
 		createTestApp("test", bundleIdentifier:"com.test.test")
 		createTestApp("excluded", bundleIdentifier:"com.test.excluded")
 		createTestApp("blacklisted", bundleIdentifier:"com.test.blacklisted")
+
+		let fileManager = NSFileManager.defaultManager()
+		fileManager.copyItemAtPath(utilDir.stringByAppendingPathComponent("hello1"), toPath: testDir.stringByAppendingPathComponent("hello1"), error: nil)
+		fileManager.copyItemAtPath(utilDir.stringByAppendingPathComponent("hello2"), toPath: testDir.stringByAppendingPathComponent("hello2"), error: nil)
+		fileManager.copyItemAtPath(utilDir.stringByAppendingPathComponent("hello3"), toPath: testDir.stringByAppendingPathComponent("hello3"), error: nil)
 	}
 
     override func tearDown() {
@@ -81,4 +90,57 @@ class Helper_Tests: XCTestCase {
 			}
 		}
     }
+
+	private func assertFileSize(path: String, expectedSize: Int, message: String) {
+		if let attributes = NSFileManager.defaultManager().attributesOfItemAtPath(path, error: nil), size = attributes[NSFileSize] as? Int {
+			XCTAssertEqual(size, expectedSize, message)
+		} else {
+			XCTAssert(false, "Could not file size of '\(path)'")
+		}
+	}
+
+	func testRemoveArchitectures() {
+		let request = HelperRequest()
+		request.dryRun = false
+		request.uid = getuid()
+		request.trash = false
+		request.includes = [ testDir ]
+		request.excludes = [ testDir.stringByAppendingPathComponent("excluded.app") ]
+		request.thin = [ "i386" ]
+		request.bundleBlacklist = [ "com.test.blacklisted" ]
+
+		let hello1Path = testDir.stringByAppendingPathComponent("hello1")
+		let hello2Path = testDir.stringByAppendingPathComponent("hello2")
+		let hello3Path = testDir.stringByAppendingPathComponent("hello3")
+		assertFileSize(hello1Path, expectedSize: 4312, message: "non-fat file size mismatch")
+		assertFileSize(hello2Path, expectedSize: 16600, message: "2-arch fat file size mismatch")
+		assertFileSize(hello3Path, expectedSize: 24792, message: "3-arch fat file size mismatch")
+
+		let expectation = expectationWithDescription("Asynchronous helper processing")
+
+		let helper = Helper()
+		helper.processRequest(request, progress: nil) { (exitCode) -> Void in
+			XCTAssert(exitCode == 0, "Helper should return with exit code 0")
+
+			let fileManager = NSFileManager.defaultManager()
+			XCTAssert(fileManager.fileExistsAtPath(self.testDir.stringByAppendingPathComponent("hello1")), "non-fat file should be present")
+			XCTAssert(fileManager.fileExistsAtPath(self.testDir.stringByAppendingPathComponent("hello2")), "2-arch fat file should be present")
+			XCTAssert(fileManager.fileExistsAtPath(self.testDir.stringByAppendingPathComponent("hello3")), "3-arch fat file should be present")
+
+			// should remain untouched
+			self.assertFileSize(hello1Path, expectedSize: 4312, message: "non-fat file size mismatch after lipo")
+			// should now be a thin (non-fat) file containing a single architecture
+			self.assertFileSize(hello2Path, expectedSize: 4312, message: "2-arch fat file size mismatch after lipo")
+			// should be a fat file containing two remaining architectures
+			self.assertFileSize(hello3Path, expectedSize: 16600, message: "3-arch fat file size mismatch after lipo")
+
+			expectation.fulfill()
+		}
+
+		waitForExpectationsWithTimeout(NSTimeInterval(5.0)) { (error) -> Void in
+			if let error = error {
+				XCTFail("Expectation failed with error: \(error)")
+			}
+		}
+	}
 }
