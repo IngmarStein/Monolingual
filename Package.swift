a6aecee59e9a588a5f5b5ef2661006e9af9bd1cf
IngmarStein <IngmarStein@users.noreply.github.com>
2016-04-15 09:23:52 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
 	],
 	dependencies: [
 		//.Package(url: "https://github.com/jatoben/CommandLine.git", versions: Version(2,2,0,prereleaseIdentifiers:["pre1"])..<Version(3,0,0)),
-		.Package(url: "https://github.com/IngmarStein/CommandLine.git", versions: Version(2,2,0,prereleaseIdentifiers:["pre2"])..<Version(3,0,0)),
-		.Package(url: "https://github.com/IngmarStein/SMJobKit.git", versions: Version(0,0,7)..<Version(1,0,0))
+		.Package(url: "https://github.com/IngmarStein/CommandLine.git", versions: Version(2,2,0,prereleaseIdentifiers:["pre3"])..<Version(3,0,0)),
+		.Package(url: "https://github.com/IngmarStein/SMJobKit.git", versions: Version(0,0,8)..<Version(1,0,0))
 	]
 )
