62001dce1ed0ed60b8d476b634a55e72e5672095
Ingmar Stein <IngmarStein@users.noreply.github.com>
2016-04-16 16:50:28 +0200

    Try use Swift 3 on Travis

---
+++ scripts/install-swift.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ev
+SWIFT_SNAPSHOT="swift-DEVELOPMENT-SNAPSHOT-2016-04-12-a"
+
+echo "Installing ${SWIFT_SNAPSHOT}..."
+if [ ! -f "${SWIFT_SNAPSHOT}-ubuntu14.04.tar.gz" ]; then
+  curl -s -L -O "https://swift.org/builds/development/xcode/${SWIFT_SNAPSHOT}/${SWIFT_SNAPSHOT}-osx.pkg"
+fi
+
+sudo installer -package "${SWIFT_SNAPSHOT}-osx.pkg" -target /
+rm -f "${SWIFT_SNAPSHOT}-osx.pkg"
