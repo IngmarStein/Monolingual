85e80f53417f66b837a7151c0acb846b3afc92d0
Ingmar Stein <IngmarStein@users.noreply.github.com>
2016-04-24 15:35:12 +0200

    Use new function signature to conform to NSXPCConnection

--- XPCService/main.swift
+++ XPCService/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 final class ServiceDelegate : NSObject, NSXPCListenerDelegate {
-	func listener(listener: NSXPCListener, shouldAcceptNewConnection newConnection: NSXPCConnection) -> Bool {
+	func listener(_ listener: NSXPCListener, shouldAcceptNewConnection newConnection: NSXPCConnection) -> Bool {
 		newConnection.exportedInterface = NSXPCInterface(with: XPCServiceProtocol.self)
 		let exportedObject = XPCService()
 		newConnection.exportedObject = exportedObject
