bea315b9d13487b7f62d8c96fa80abac3368c792
IngmarStein <IngmarStein@users.noreply.github.com>
2016-05-09 16:09:59 +0200

    Tailor: include Package.swift

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 		Target(name: "lipo")
 	],
 	dependencies: [
-		//.Package(url: "https://github.com/jatoben/CommandLine.git", versions: Version(2,2,0,prereleaseIdentifiers:["pre1"])..<Version(3,0,0)),
-		.Package(url: "https://github.com/IngmarStein/CommandLine.git", versions: Version(2,2,0,prereleaseIdentifiers:["pre3"])..<Version(3,0,0)),
-		.Package(url: "https://github.com/IngmarStein/SMJobKit.git", versions: Version(0,0,8)..<Version(1,0,0))
+		// .Package(url: "https://github.com/jatoben/CommandLine.git", versions: Version(2, 2, 0, prereleaseIdentifiers: ["pre1"])..<Version(3, 0, 0)),
+		.Package(url: "https://github.com/IngmarStein/CommandLine.git", versions: Version(2, 2, 0, prereleaseIdentifiers: ["pre3"])..<Version(3, 0, 0)),
+		.Package(url: "https://github.com/IngmarStein/SMJobKit.git", versions: Version(0, 0, 8)..<Version(1, 0, 0))
 	]
 )
