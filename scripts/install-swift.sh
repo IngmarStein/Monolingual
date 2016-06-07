b77d6bec1080546901a2b9948f43f6df96bf2937
Ingmar Stein <IngmarStein@users.noreply.github.com>
2016-06-07 06:51:24 +0200

    Update to Swift 2016-06-06

--- scripts/install-swift.sh
+++ scripts/install-swift.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-SWIFT_SNAPSHOT="swift-DEVELOPMENT-SNAPSHOT-2016-05-31-a"
+SWIFT_SNAPSHOT="swift-DEVELOPMENT-SNAPSHOT-2016-06-06-a"
 
 echo "Installing ${SWIFT_SNAPSHOT}..."
 if [ ! -f "${SWIFT_SNAPSHOT}-osx.pkg" ]; then
