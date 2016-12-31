78c6f4878a049919d7e36b4a78a86fae4072c647
Ingmar Stein <IngmarStein@users.noreply.github.com>
2016-12-31 16:34:40 +0100

    Bump copyright year

--- Sources/PreferencesViewController.swift
+++ Sources/PreferencesViewController.swift
@@ -3,7 +3,7 @@
 //  Monolingual
 //
 //  Created by Ingmar Stein on Mon Apr 19 2004.
-//  Copyright (c) 2004-2016 Ingmar Stein. All rights reserved.
+//  Copyright (c) 2004-2017 Ingmar Stein. All rights reserved.
 //
 
 import Cocoa
