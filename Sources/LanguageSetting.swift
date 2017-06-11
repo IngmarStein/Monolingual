0add498ccd2eb962d6e61501a3749c5a3dd04635
Ingmar Stein <IngmarStein@users.noreply.github.com>
2017-06-11 22:57:30 +0200

    Enable new build system

--- Sources/LanguageSetting.swift
+++ Sources/LanguageSetting.swift
@@ -10,7 +10,7 @@ import Foundation
 
 final class LanguageSetting: Setting {
 	var folders: [String]
-	var displayName: String
+	@objc var displayName: String
 
 	init(enabled: Bool, folders: [String], displayName: String) {
 		self.folders = folders
