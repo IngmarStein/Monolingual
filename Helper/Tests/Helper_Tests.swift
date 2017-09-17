598f8e1b03a126a2ac4e7929a5b02726c4c45d34
Ingmar Stein <IngmarStein@users.noreply.github.com>
2017-09-17 11:22:36 +0200

    Fix helper test compilation

--- Helper/Tests/Helper_Tests.swift
+++ Helper/Tests/Helper_Tests.swift
@@ -89,7 +89,7 @@ class HelperTests: XCTestCase {
 		let helperExpectation = expectation(description: "Asynchronous helper processing")
 
 		let helper = Helper()
-		helper.processRequest(request) { (exitCode) -> Void in
+		helper.process(request: request) { (exitCode) -> Void in
 			XCTAssert(exitCode == 0, "Helper should return with exit code 0")
 
 			let fileManager = FileManager.default
@@ -144,7 +144,7 @@ class HelperTests: XCTestCase {
 		let helperExpectation = expectation(description: "Asynchronous helper processing")
 
 		let helper = Helper()
-		helper.processRequest(request) { (exitCode) -> Void in
+		helper.process(request: request) { (exitCode) -> Void in
 			XCTAssert(exitCode == 0, "Helper should return with exit code 0")
 
 			let fileManager = FileManager.default
