04c7db17efebfac2de3a3695c2b920589eadd44a
Ingmar Stein <IngmarStein@users.noreply.github.com>
2018-03-12 21:14:07 +0100

    fastlane: set output directory

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -22,11 +22,11 @@ class Fastfile: LaneFile {
 
 	func betaLane() {
 		desc("Build a new beta version with debug information")
-		buildApp(workspace: "Monolingual.xcworkspace", scheme: "Monolingual", configuration: "Debug", outputDirectory: "./build")
+		buildApp(workspace: "Monolingual.xcworkspace", scheme: "Monolingual", outputDirectory: "./build", configuration: "Debug")
 	}
 
 	func releaseLane() {
 		desc("Build a new release version")
-		buildApp(workspace: "Monolingual.xcworkspace", scheme: "Monolingual", clean: true, configuration: "Release", outputDirectory: "./build")
+		buildApp(workspace: "Monolingual.xcworkspace", scheme: "Monolingual", clean: true, outputDirectory: "./build", configuration: "Release")
 	}
 }
