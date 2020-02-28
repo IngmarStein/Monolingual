9010bb2bd177b85a1379ee474fb8189349b31b4a
Ingmar Stein <IngmarStein@users.noreply.github.com>
2020-02-28 20:58:35 +0100

    Replace IngmarStein/CommandLineKit with apple/swift-argument-parser

--- Helper/Sources/main.swift
+++ Helper/Sources/main.swift
@@ -7,56 +7,69 @@
 //
 
 import Foundation
-import CommandLineKit
+import ArgumentParser
 
-private let cli = CommandLine()
-private let uninstall = BoolOption(shortFlag: "u", longFlag: "uninstall", helpMessage: "Uninstall helper.")
-private let version = BoolOption(shortFlag: "v", longFlag: "version", helpMessage: "Print version and exit.")
-private let dryRun = BoolOption(shortFlag: "n", longFlag: "dry", helpMessage: "Dry run: don't make any changes to the filesystem.")
-private let strip = BoolOption(shortFlag: "s", longFlag: "strip", helpMessage: "Strip debug info from executables.")
-private let trash = BoolOption(shortFlag: "t", longFlag: "trash", helpMessage: "Don't delete files but move them to the trash.")
-private let includes = MultiStringOption(shortFlag: "i", longFlag: "include", required: false, helpMessage: "Include directory.")
-private let excludes = MultiStringOption(shortFlag: "x", longFlag: "exclude", required: false, helpMessage: "Exclude directory.")
-private let bundles = MultiStringOption(shortFlag: "b", longFlag: "bundle", required: false, helpMessage: "Exclude a bundle from processing (e.g. \"com.apple.iPhoto\").")
-private let delete = MultiStringOption(shortFlag: "d", longFlag: "delete", required: false, helpMessage: "Name of a file or directory to delete (e.g. \"fr.lproj\").")
-private let thin = MultiStringOption(shortFlag: "a", longFlag: "thin", required: false, helpMessage: "Remove architecture from universal binary (e.g. \"ppc\").")
+struct Options: ParsableArguments {
+	@Flag(name: .shortAndLong, help: "Uninstall helper.")
+	var uninstall: Bool
 
-cli.addOptions(uninstall, version, dryRun, strip, trash, includes, excludes, bundles, delete, thin)
+	@Flag(name: .shortAndLong, help: "Print version and exit.")
+	var version: Bool
 
-do {
-	try cli.parse(strict: true)
+	@Flag(name: [.customShort("n"), .long], help: "Dry run: don't make any changes to the filesystem.")
+	var dryRun: Bool
 
-	let helper = Helper()
+	@Flag(name: .shortAndLong, help: "Strip debug info from executables.")
+	var strip: Bool
 
-	if uninstall.value {
-		helper.uninstall()
-		exit(EXIT_SUCCESS)
+	@Flag(name: .shortAndLong, help: "Don't delete files but move them to the trash.")
+	var trash: Bool
+
+	@Option(name: .shortAndLong, help: "Include directory.")
+	var include: [String]
+
+	@Option(name: [.customShort("x"), .long], help: "Exclude directory.")
+	var exclude: [String]
+
+	@Option(name: .shortAndLong, help: "Exclude a bundle from processing (e.g. \"com.apple.iPhoto\").")
+	var bundle: [String]
+
+	@Option(name: .shortAndLong, help: "Name of a file or directory to delete (e.g. \"fr.lproj\").")
+	var delete: [String]
+
+	@Option(name: [.customShort("a"), .long], help: "Remove architecture from universal binary (e.g. \"ppc\").")
+	var thin: [String]
+}
+
+
+let options = Options.parseOrExit()
+
+let helper = Helper()
+
+if options.uninstall {
+	helper.uninstall()
+	exit(EXIT_SUCCESS)
+}
+
+if options.version {
+	print("MonolingualHelper version \(helper.version)")
+	exit(EXIT_SUCCESS)
+}
+
+if options.include.isEmpty {
+	helper.run()
+} else {
+	let request = HelperRequest()
+	request.dryRun = options.dryRun
+	request.doStrip = options.strip
+	request.trash = options.trash
+	request.includes = options.include
+	request.excludes = options.exclude
+	request.bundleBlacklist = Set<String>(options.bundle)
+	request.directories = Set<String>(options.delete)
+	request.thin = options.thin
+	helper.process(request: request, progress: nil) { (result) -> Void in
+		exit(Int32(result))
 	}
-
-	if version.value {
-		print("MonolingualHelper version \(helper.version)")
-		exit(EXIT_SUCCESS)
-	}
-
-	if includes.wasSet {
-		let request = HelperRequest()
-		request.dryRun = dryRun.value
-		request.doStrip = strip.value
-		request.trash = trash.value
-		request.includes = includes.value
-		request.excludes = excludes.value
-		request.bundleBlacklist = bundles.value.flatMap { Set<String>($0) }
-		request.directories = delete.value.flatMap { Set<String>($0) }
-		request.thin = thin.value
-		helper.process(request: request, progress: nil) { (result) -> Void in
-			exit(Int32(result))
-		}
-		RunLoop.current.run()
-	} else {
-		helper.run()
-	}
-} catch {
-	print(error)
-	cli.printUsage()
-	exit(EX_USAGE)
+	RunLoop.current.run()
 }
